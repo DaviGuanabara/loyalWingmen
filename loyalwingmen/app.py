db1dfd57db8517c5c5009fb6cac16b1d971e1e0d
DaviGuanabara <davi_guanabara@live.com>
2023-03-29 20:21:24 -0300

    drone aparecendo

--- loyalwingmen/app.py
+++ loyalwingmen/app.py
@@ -25,35 +25,30 @@ from stable_baselines3.common.monitor import Monitor
 
 from stable_baselines3.common.env_checker import check_env
 
-#env = MyFirstEnv() #gym.make('MyFirstEnv-v0')
-#env = my_first_env()
-#check_env(env)
-
-n_envs = 2
-#env = make_vec_env("envs/MyFirstEnv", n_envs=2)
-env = make_vec_env(MyFirstEnv, n_envs=n_envs)
-#tenho que remover o GUI caso queria usar múltimos ambientes.
+train = False
+test = True
 
 
+if train:
 
-eval_callback = callbacklist(env, log_path="./logs/", model_path="./models/",
-                             n_envs=n_envs, save_freq=10_000)
+    n_envs = 2
 
+    env = make_vec_env(MyFirstEnv, n_envs=n_envs)
+    eval_callback = callbacklist(env, log_path="./logs/", model_path="./models/",
+                                n_envs=n_envs, save_freq=10_000)
 
+    model = PPO("MlpPolicy", env, verbose=0, device='auto')  # + "/" str(learning_rate) +
+    # reset_num_timesteps=False,
+    model.learn(total_timesteps=10_000, callback=eval_callback)
 
-# model = PPO("MlpPolicy", env, verbose=1, device='auto', policy_kwargs=policy_kwargs, n_steps=n_steps, batch_size=batch_size, learning_rate=learning_rate, tensorboard_log=base_log_path) #+ topology_name) #+ "/" str(learning_rate) +
-model = PPO("MlpPolicy", env, verbose=0, device='auto')  # + "/" str(learning_rate) +
+if test:     
 
+    env = MyFirstEnv(GUI=True)
+    for steps in range(5000):
+        # agent policy that uses the observation and info
+        #action = env.action_space.sample()
+        observation, reward, done, info = env.step([])
 
-# reset_num_timesteps=False,
-model.learn(total_timesteps=10_000, callback=eval_callback) 
-
-env = MyFirstEnv(GUI=True)
-for steps in range(1000):
-    # agent policy that uses the observation and info
-    #action = env.action_space.sample()
-    observation, reward, done, info = env.step([])
-
-    if done:
-        
-        env.reset()
+        if done:
+            #print("done")
+            env.reset()
--- loyalwingmen/envs/my_first_env.py
+++ loyalwingmen/envs/my_first_env.py
@@ -73,8 +73,9 @@ class MyFirstEnv(gym.Env):
         #### Options ###############################################
         self.DRONE_MODEL = drone_model
         self.PHYSICS = physics
-        self.URDF = self.DRONE_MODEL.value + ".urdf"
-      
+        self.URDF = str(drone_model.value + ".urdf")
+        
+        
   
 
         self.step_counter = 0
@@ -109,6 +110,10 @@ class MyFirstEnv(gym.Env):
             # if platform == "linux": p.setAdditionalSearchPath(pybullet_data.getDataPath()); plugin = p.loadPlugin(egl.get_filename(), "_eglRendererPlugin"); print("plugin=", plugin)
             
         #### Set initial poses #####################################
+
+        self._parseURDFParameters(drone_model.value + ".urdf")
+        string = str(drone_model.value + ".urdf")
+        self.load_agent(string)
        
         #### Create action and observation spaces ##################
         self.action_space = self._actionSpace()
@@ -117,6 +122,15 @@ class MyFirstEnv(gym.Env):
         #self._housekeeping()
         #### Update and store the drones kinematic information #####
         #self._updateAndStoreKinematicInformation()
+
+    def load_agent(self, urdf_file_name: str, initial_position=np.ones(3), initial_angular_position=np.zeros(3)):
+        urdf_file_path = 'assets/' + urdf_file_name
+        p.loadURDF(urdf_file_path,
+                initial_position, p.getQuaternionFromEuler(initial_angular_position),
+                                         flags=p.URDF_USE_INERTIA_FROM_FILE,
+                                          physicsClientId=self.CLIENT
+                                          ) 
+
 
         
 
@@ -149,6 +163,9 @@ class MyFirstEnv(gym.Env):
             in each subclass for its format.
         """
         p.resetSimulation(physicsClientId=self.CLIENT)
+
+        self.load_agent(self.URDF)
+
         #### Housekeeping ##########################################
         initial_xyzs = np.zeros(3)
         
@@ -465,3 +482,37 @@ class MyFirstEnv(gym.Env):
         """
         #raise NotImplementedError
         return {}
+
+    def _parseURDFParameters(self, urdf_file_path):
+        """Loads parameters from an URDF file.
+        This method is nothing more than a custom XML parser for the .urdf
+        files in folder `assets/`.
+        """
+        URDF_TREE = etxml.parse(pkg_resources.resource_filename(
+            'gym_pybullet_drones', 'assets/' + urdf_file_path)).getroot()
+        M = float(URDF_TREE[1][0][1].attrib['value'])
+        L = float(URDF_TREE[0].attrib['arm'])
+        THRUST2WEIGHT_RATIO = float(URDF_TREE[0].attrib['thrust2weight'])
+        IXX = float(URDF_TREE[1][0][2].attrib['ixx'])
+        IYY = float(URDF_TREE[1][0][2].attrib['iyy'])
+        IZZ = float(URDF_TREE[1][0][2].attrib['izz'])
+        J = np.diag([IXX, IYY, IZZ])
+        J_INV = np.linalg.inv(J)
+        KF = float(URDF_TREE[0].attrib['kf'])
+        KM = float(URDF_TREE[0].attrib['km'])
+        COLLISION_H = float(URDF_TREE[1][2][1][0].attrib['length'])
+        COLLISION_R = float(URDF_TREE[1][2][1][0].attrib['radius'])
+        COLLISION_SHAPE_OFFSETS = [
+            float(s) for s in URDF_TREE[1][2][0].attrib['xyz'].split(' ')]
+        COLLISION_Z_OFFSET = COLLISION_SHAPE_OFFSETS[2]
+        MAX_SPEED_KMH = float(URDF_TREE[0].attrib['max_speed_kmh'])
+        GND_EFF_COEFF = float(URDF_TREE[0].attrib['gnd_eff_coeff'])
+        PROP_RADIUS = float(URDF_TREE[0].attrib['prop_radius'])
+        DRAG_COEFF_XY = float(URDF_TREE[0].attrib['drag_coeff_xy'])
+        DRAG_COEFF_Z = float(URDF_TREE[0].attrib['drag_coeff_z'])
+        DRAG_COEFF = np.array([DRAG_COEFF_XY, DRAG_COEFF_XY, DRAG_COEFF_Z])
+        DW_COEFF_1 = float(URDF_TREE[0].attrib['dw_coeff_1'])
+        DW_COEFF_2 = float(URDF_TREE[0].attrib['dw_coeff_2'])
+        DW_COEFF_3 = float(URDF_TREE[0].attrib['dw_coeff_3'])
+        return M, L, THRUST2WEIGHT_RATIO, J, J_INV, KF, KM, COLLISION_H, COLLISION_R, COLLISION_Z_OFFSET, MAX_SPEED_KMH, \
+            GND_EFF_COEFF, PROP_RADIUS, DRAG_COEFF, DW_COEFF_1, DW_COEFF_2, DW_COEFF_3
