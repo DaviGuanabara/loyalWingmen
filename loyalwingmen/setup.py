44097ccc6be4641d23acc32463f3b74a244eaefa
DaviGuanabara <davi_guanabara@live.com>
2023-05-11 05:16:36 -0300

    Update setup.py

--- loyalwingmen/setup.py
+++ loyalwingmen/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="loyalwingmen",
     version="0.0.1",
-    #install_requires=['opencv-python', 'numpy', 'pybullet', 'pytest', 'pygame', "gym"],
+    #install_requires=['opencv-python', 'numpy', 'pybullet', 'pytest', 'pygame', "gym[all]"],
     install_requires=[]
 )
 
@@ -11,3 +11,15 @@ setup(
 
 #to execute:
 # GitHub\loyalWingmen> pip install -e loyal_wingmen
+# sdl2 sdl2_image sdl2_mixer sdl2_ttf pkg-config
+#pip install wheel
+# swig, cmake, stable_baselines3[extra]
+# python3 -m pip install tensorflow
+# pip install git+https://github.com/carlosluis/stable-baselines3@fix_tests
+# gym sem o all
+# python-tk (tkinter)
+# openpyxl
+# pip install stable-baselines3[extra]
+# pynput
+# shimmy
+# tem q usar o gymnasium ou algo do tipo o nome
