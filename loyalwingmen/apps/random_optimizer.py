7c6e1e6b84abcab0644569c0170394f1bf7b2f3d
DaviGuanabara <davi_guanabara@live.com>
2023-06-04 11:00:10 -0300

    Update random_optimizer.py

--- loyalwingmen/apps/random_optimizer.py
+++ loyalwingmen/apps/random_optimizer.py
@@ -46,60 +46,22 @@ class Training:
         n_repetitions=2,
         total_rl_steps=2_000_000,
     ):  
-        #https://github.com/openai/gym/issues/1542
-        #problemas com o async 
-
+        
         rewards = np.array([])
         for i in range(n_repetitions):
 
             
-# get the number of logical cpu cores
-            n_cores = cpu_count()
-            print(n_cores)
-            n_envs = n_cores
+            number_of_logical_cores = cpu_count()
+            n_envs = number_of_logical_cores
             nn_t = topology
 
-            #https://towardsdatascience.com/a-journey-towards-faster-reinforcement-learning-1c97b2cc32e1
-            #boa discurssão sobre o batch size
 
-            #https://github.com/DLR-RM/stable-baselines3/issues/121
-            #uma boa discurssão sobre o número de ambientes.
-
-            #https://discuss.pytorch.org/t/pytorch-keeps-using-cpu-instead-of-gpu/131690/2
-            #outra discurssão boa sobre o gargalo ser a cpu
-
-            #https://towardsdatascience.com/a-journey-towards-faster-reinforcement-learning-1c97b2cc32e1
-            #https://benjaminwarner.dev/2022/08/31/training-atari-agents-faster
-            #sobre a velocidade do treinamento
-
-            #https://stable-baselines3.readthedocs.io/en/master/guide/vec_envs.html
-            # tá entrando como Dummy, mas o Dummy não suporta multiprocessing, que é o caso
-            #classstable_baselines3.common.vec_env.SubprocVecEnv(env_fns, start_method=None)[source]
-            #tenho que usar esse aí de cima.
-            #For performance reasons, if your environment is not IO bound, the number of environments should not exceed the number of logical cores on your CPU.
-            #
-            #
-            #vectorized_environment = make_vec_env(DroneAndCube, n_envs=n_envs)
-            #vectorized_environment = gym.vector.AsyncVectorEnv([
-            #    lambda: DroneAndCube(),
-            #    lambda: DroneAndCube()
-            #])
-
-            #Creates a multiprocess vectorized wrapper for multiple environments, distributing each environment to its own process, allowing significant speed up when the environment is computationally complex.
-            #For performance reasons, if your environment is not IO bound, the number of environments should not exceed the number of logical cores on your CPU.
-            #
-
-            #[make_env(env_id, i+total_procs) for i in range(n_procs)]
             env_fns = []
             for _ in range(n_envs):
                 env_fns.append(DroneAndCube)
 
 
             vectorized_environment = SubprocVecEnv(env_fns)
-
-            print("n_envs:", n_envs)
-            # print(vectorized_environment.env_method("get_parameteres")[0])
-            # aggregate_physics_steps = vectorized_environment.env_method("get_parameteres")[0].aggregate_physics_steps
 
             callback_list, storage_for_callback = callbacklist(
                 vectorized_environment,
@@ -109,9 +71,7 @@ class Training:
                 save_freq=30_000,
             )
 
-            # TODO isso daqui tá dando pau. Então, ajeitar
-            # UserWarning: As shared layers in the mlp_extractor are removed since SB3 v1.8.0,
-            # you should now pass directly a dictionary and not a list (net_arch=dict(pi=..., vf=...) instead of net_arch=[dict(pi=..., vf=...)])
+            
             policy_kwargs = dict(
                 activation_fn=th.nn.LeakyReLU, net_arch=dict(pi=nn_t, vf=nn_t)
             )
@@ -126,7 +86,7 @@ class Training:
                 tensorboard_log="./logs/" + log_name + "/",
                 policy_kwargs=policy_kwargs,
                 learning_rate=learning_rate,
-                batch_size=2048 #normalmente varia entre 32 e 2048
+                batch_size=2048 #from 32 up to 2048
             )
 
             model.learn(
@@ -137,19 +97,18 @@ class Training:
 
             rewards = np.append(rewards, storage_for_callback.best_mean_reward)
 
-            # print(storage_for_callback)
-        return np.mean(rewards)  # storage_for_callback
+        return np.mean(rewards)
 
 
 def gen_topology(min_layers, max_layers):
     topology = np.array([]).astype("int32")
     for _ in range(randint(min_layers, max_layers)):  # número de camadas variáveis
-        topology = np.append(topology, choice([256, 512, 1024]))  # 256, 512, 1024
+        topology = np.append(topology, choice([256, 512, 1024]))
     return topology
 
 
 def save(df, topology, reward, index, learning_rate, total_rl_steps, n_repetitions):
-    # TODO aqui está a forma que eu vou salvar o treinamento. O mewlhor seria tamb[em salvar os logs e os modelos treinados de uma forma bonita. Enfim.
+    
 
     df = pd.concat(
         [
