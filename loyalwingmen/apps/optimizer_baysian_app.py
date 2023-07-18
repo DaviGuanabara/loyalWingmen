49b90e25a481dfd6ccd064032cea13d17b3fa315
DaviGuanabara <davi_guanabara@live.com>
2023-07-18 20:06:27 -0300

    Update optimizer_baysian_app.py

--- loyalwingmen/apps/optimizer_baysian_app.py
+++ loyalwingmen/apps/optimizer_baysian_app.py
@@ -16,11 +16,35 @@ from datetime import datetime
 import optuna
 from optuna.trial import Trial
 from optuna.samplers import TPESampler
+from openpyxl import load_workbook, Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+import warnings
+
+# Ignorar o aviso específico
+#warnings.filterwarnings("ignore", message="WARN: Box bound precision lowered by casting to float32")
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Seu código aqui
+
 
 # Configurando o logging
-# TODO: A new study created in memory with name: no-name-4ae93828-1f18-48da-a74d-e18b574987b3 
-#WHO IS THIS GUY??
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def create_output_folder(experiment_name: str):
+    # Obter a data atual
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Criar o nome da pasta com base na data atual
+    #folder_name = f"output/{experiment_name}"
+    folder_name = os.path.join("output", experiment_name)
+
+    # Criar a pasta se ainda não existir
+    os.makedirs(folder_name, exist_ok=True)
+
+    # Retornar o caminho completo para a pasta
+    return folder_name
+
 
 def create_vectorized_environment(n_envs: int, frequency: int) -> SubprocVecEnv:
     env_fns = [DemoEnvironment for _ in range(n_envs)]
@@ -28,14 +52,14 @@ def create_vectorized_environment(n_envs: int, frequency: int) -> SubprocVecEnv:
     vectorized_environment.env_method("set_frequency", 240, frequency, indices=None)
     return vectorized_environment
 
-def create_callback_list(n_envs: int, vectorized_environment: SubprocVecEnv, callbacks_to_include: List[str]):
-    log_path = "./logs/"
-    model_path = "./models/"
+def create_callback_list(n_envs: int, vectorized_environment: SubprocVecEnv, callbacks_to_include: List[str], model_dir: str, log_dir: str):
+    #log_path = "./logs/"
+    #model_path = "./models/"
     save_freq = 100_000
     callback_list, storage_for_callback = callbacklist(
         vectorized_environment,
-        log_path=log_path,
-        model_path=model_path,
+        log_path=log_dir,
+        model_path=model_dir,
         n_envs=n_envs,
         save_freq=save_freq,
         callbacks_to_include=callbacks_to_include
@@ -68,23 +92,33 @@ def train_model(model: PPO, callback_list, n_timesteps: int = 1_000_000) -> PPO:
     model.learn(total_timesteps=n_timesteps, callback=callback_list)
     return model
 
-def save_model(model: PPO, model_path: str):
+def save_model(model: PPO, hiddens: list, frequency: int, learning_rate: float, output_folder: str):
+    folder_name = f"model_{hiddens}_{frequency}_{learning_rate}"
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_dir = os.path.join(output_folder, f"models_{folder_name}_{current_time}")
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "my_model")
     model.save(model_path)
     logging.info(f"Model saved at: {model_path}")
 
-def cross_validation_simulation(hiddens: list, frequency: int, learning_rate: float, num_folds: int) -> float:
+
+def cross_validation_simulation(hiddens: list, frequency: int, learning_rate: float, num_folds: int, output_folder: str, n_timesteps: int) -> float:
     number_of_logical_cores = os.cpu_count()
     n_envs = number_of_logical_cores
 
     vectorized_environment = create_vectorized_environment(n_envs, frequency)
     vectorized_environment = VecMonitor(vectorized_environment)
     
-    callback_list, _ = create_callback_list(n_envs, vectorized_environment, callbacks_to_include=["progressbar"])
+    #excel_folder_path = os.path.join(output_folder, 'simulation_results.xlsx')
+    model_dir = os.path.join(output_folder, 'models')
+    log_dir = os.path.join(output_folder, 'logs')
+    callback_list, _ = create_callback_list(n_envs, vectorized_environment, callbacks_to_include=["progressbar"], model_dir=model_dir, log_dir=log_dir)
     policy_kwargs = create_policy_kwargs(hiddens, learning_rate)
     model = create_model(vectorized_environment, policy_kwargs, learning_rate)
 
     logging.info(model.policy)
-    model = train_model(model, callback_list)
+    model = train_model(model, callback_list, n_timesteps)
+    save_model(model, hiddens, frequency, learning_rate, output_folder)
     avg_score, std_dev = evaluate_policy(model, vectorized_environment, n_eval_episodes=num_folds, deterministic=True)
     logging.info(f"Avg score: {avg_score}")
     return avg_score
@@ -97,79 +131,76 @@ def generate_random_parameters() -> Tuple[list, int, float]:
     learning_rate = uniform(0.00000000001, 0.1).rvs() 
     return hiddens, frequency, learning_rate
 
-def objective(trial: Trial) -> float:
-    hiddens = trial.suggest_int('hiddens', 10, 1000)
-    num_hiddens = trial.suggest_int('num_hiddens', 3, 8)
-    hiddens = [trial.suggest_int(f'hiddens_{i}', 10, 1000) for i in range(num_hiddens)]
-    frequency = trial.suggest_int('frequency', 1, 8) * 15 #Explanation: 1, 2, 3, 4, 5, 6, 7, 8 -> 15, 30, 45, 60, 75, 90, 105, 120
-    learning_rate = trial.suggest_float('learning_rate', 0.00000000001, 0.1)
+
+
+
+
+def save_results_to_excel(results, output_folder, file_name):
+    # Construir o caminho completo do arquivo
+    file_path = os.path.join(output_folder, file_name)
+
+    # Verificar se o diretório de saída existe
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Verificar se o arquivo existe
+    if not os.path.isfile(file_path):
+        # Se o arquivo não existe, criar uma nova planilha
+        workbook = Workbook()
+        sheet: Worksheet = workbook.active
+        
+        # Definir os cabeçalhos das colunas
+        sheet.append(['hiddens', 'frequency', 'learning_rate', 'score'])    
+    else:
+        # Carregar a planilha existente
+        workbook = load_workbook(file_path)
+        # Obter a planilha ativa
+        sheet: Worksheet = workbook.active
+        
     
-    #TODO melhorar o nome num_folds. 
-    #TODO melhorar o nome cross_validation_simulation
-    avg_score = cross_validation_simulation(hiddens, frequency, learning_rate, num_folds=100)
+        
+    for result in results:
+        hiddens_str = ', '.join(str(x) for x in result[0])
+        sheet.append([hiddens_str, result[1], result[2], result[3]])
+
+    try:
+        # Salvar as alterações no arquivo
+        workbook.save(file_path)
+    except PermissionError:
+        print(f"Não foi possível salvar o arquivo '{file_name}'. Permissão negada.")
+    except Exception as e:
+        print(f"Erro ocorrido ao salvar os resultados: {e}")    
+
+    # Adicionar os novos resultados
+    #for result in results:
+    #    sheet.append(result)
+
+
+
+
+
+
+def objective(trial: Trial, output_folder: str, n_timesteps: int) -> float:
+    
+    num_hiddens = trial.suggest_int('num_hiddens', 3, 8)
+    hiddens = [trial.suggest_int(f'hiddens_{i}', 100, 1000) for i in range(num_hiddens)]
+    frequency = trial.suggest_int('frequency', 1, 8) * 15
+    exponent = trial.suggest_uniform('exponent', -10, -1)
+    learning_rate = 10 ** exponent
+
+    
+    print("Parameters:")
+    print("Hiddens: ", hiddens)
+    print("Frequency: ", frequency)
+    print("Learning Rate: ", learning_rate)
+
+    avg_score = cross_validation_simulation(hiddens, frequency, learning_rate, num_folds=5, output_folder=output_folder, n_timesteps=n_timesteps)
+
+    # Salvar os resultados na planilha
+    result = (hiddens, frequency, learning_rate, avg_score)
+    save_results_to_excel([result], output_folder, 'simulation_results.xlsx')
+
     return avg_score
-
-def optimize_hyperparameters(num_iterations: int = 100) -> List[Tuple[List[int], int, float, float]]:
-    """Optimize hyperparameters using the Bayesian optimization algorithm.
-
-    Parameters
-    ----------
-    num_iterations : int
-        The number of iterations to run the algorithm for.
-
-    Returns
-    -------
-    results : List[Tuple[List[int], int, float, float]]
-        The results of each iteration, as a list of tuples containing the hiddens, frequency, learning_rate, and score.
-    """
-
-    results = []
-    best_score = float('-inf')
-    stop_counter = 0
-
-    # DataFrame para armazenar os resultados
-    df = pd.DataFrame(columns=['hiddens', 'frequency', 'learning_rate', 'score'])
-
-    for i in range(num_iterations):
-        hiddens, frequency, learning_rate = generate_random_parameters()
-        avg_score = cross_validation_simulation(hiddens, frequency, learning_rate, num_folds=5)
-
-        results.append((hiddens, frequency, learning_rate, avg_score))
-        logging.info(f"Iteration {i}, average score: {avg_score}, parameters: {hiddens, frequency, learning_rate}")
-
-        print_iteration_results(i, avg_score, hiddens, frequency, learning_rate)
-
-        df.loc[i] = [hiddens, frequency, learning_rate, avg_score]
-
-        folder_path = os.path.join("results-bayesian-optimization", 'simulation_results.xlsx')
-        with pd.ExcelWriter(folder_path, mode='a', engine='openpyxl') as writer:
-            df.to_excel(writer, sheet_name='Results', index=False, header=not writer.sheets['Results'])
-
-        if avg_score > best_score:
-            best_score = avg_score
-            stop_counter = 0
-        else:
-            stop_counter += 1
-
-        if stop_counter >= 10:
-            logging.info(f"Stopping early at iteration {i}")
-            break
-
-    return results
-
-def print_iteration_results(iteration: int, avg_score: float, hiddens: List[int], frequency: int, learning_rate: float):
-    print(f"Iteration {iteration} results:")
-    print("Best score: %.4f" % avg_score)
-    print("Best parameters:")
-    print("Hiddens: %s" % ', '.join(map(str, hiddens)))
-    print("Frequency: %.4f" % frequency)
-    print("Learning rate: %.10f" % learning_rate)
-
-def save_results_to_excel(results: List[Tuple[List[int], int, float, float]]):
-    df = pd.DataFrame(results, columns=['hiddens', 'frequency', 'learning_rate', 'score'])
-    df['hiddens'] = df['hiddens'].apply(lambda x: ', '.join(map(str, x)))
-    folder_path = os.path.join("results-bayesian-optimization", 'simulation_results.xlsx')
-    df.to_excel(folder_path, index=False)
 
 def print_best_parameters(results: List[Tuple[List[int], int, float, float]]):
     results = sorted(results, key=lambda x: x[-1], reverse=True)
@@ -177,25 +208,26 @@ def print_best_parameters(results: List[Tuple[List[int], int, float, float]]):
     print("Best parameters:")
     print("Hiddens: %s" % ', '.join(map(str, results[0][0])))
     print("Frequency: %.4f" % results[0][1])
-    print("Learning rate: %.10f" % results[0][2])    
+    print("Learning rate: %.10f" % results[0][2])
 
 def main():
+    
+    
+    n_timesteps = 1_000_000
+    experiment_name = "Optimizer_baysian_app"
+    # Criar a pasta de saída
+    output_folder = create_output_folder(experiment_name)
+
     study = optuna.create_study(direction='maximize', sampler=TPESampler())
-    study.optimize(objective, n_trials=100)
+    study.optimize(lambda trial: objective(trial, output_folder, n_timesteps), n_trials=100)
 
     results = []
     for trial in study.trials:
         hiddens = [trial.params[f'hiddens_{i}'] for i in range(trial.params['num_hiddens'])]
         results.append((hiddens, trial.params['frequency'], trial.params['learning_rate'], trial.value))
-        print("saving")
-        save_results_to_excel(results)
-        print("saved")
-        print("printing")
-        print_best_parameters(results)
-        print("printed")
 
-    
-    
+    #save_results_to_excel(results)
+    print_best_parameters(results)
 
 if __name__ == "__main__":
     main()
